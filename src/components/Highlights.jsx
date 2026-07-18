c1cc11440840de6a7f23ee3efa57d737cb7aecc9
Mohamed Taha <69488789+mohamedtaha152@users.noreply.github.com>
2026-07-18 04:24:05 +0300

    Update Highlights.jsx

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,59 +1,60 @@
+import useMacbookStore from "../store";
+import clsx from "clsx";
+import {Canvas} from "@react-three/fiber";
+import {Box, OrbitControls} from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14.jsx";
+
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from './three/ModelSwitcher.jsx'
 import {useMediaQuery} from "react-responsive";
-import {useGSAP} from "@gsap/react";
-import gsap from "gsap";
 
-const Highlights = () => {
-    const isMobile = useMediaQuery({  query: '(max-width: 1024px)' });
+const ProductViewer = () => {
+    const { color, scale, setColor, setScale } = useMacbookStore();
 
-    useGSAP(() => {
-        gsap.to(['.left-column', '.right-column'], {
-            scrollTrigger: {
-                trigger: '#highlights',
-                start: isMobile ? 'bottom bottom' : 'top top'
-            },
-            y: 0,
-            opacity: 1,
-            stagger: 0.5,
-            duration: 1,
-            ease: 'power1.inOut'
-        });
-    })
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)'});
 
     return (
-        <section id="highlights">
-            <h2>There’s never been a better time to upgrade.</h2>
-            <h3>Here’s what you get with the new MacBook Pro.</h3>
+        <section id="product-viewer">
+            <h2>Take a closer look.</h2>
 
-            <div className="masonry">
-                <div className="left-column">
-                    <div>
-                        <img src="/laptop.png" alt="Laptop" />
-                        <p>Fly through demanding tasks up to 9.8x faster.</p>
+            <div className="controls">
+                {/*<p className="info">Macbook Pro | Available in 14" & 16" in Space Gray & Dark colors</p>*/}
+
+                <div className="flex-center gap-5 mt-5">
+                    <div className="color-control">
+                        <div
+                            onClick={() => setColor('#adb5bd')}
+                            className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')}
+                        />
+                        <div
+                            onClick={() => setColor('#2e2c2e')}
+                            className={clsx('bg-neutral-900', color === '#2e2c2e' && 'active')}
+                        />
                     </div>
-                    <div>
-                        <img src="/sun.png" alt="Sun" />
-                        <p>A stunning <br />
-                            Liquid Retina XDR <br/>
-                            display.</p>
-                    </div>
-                </div>
-                <div className="right-column">
-                    <div className="apple-gradient">
-                        <img src="/ai.png" alt="AI" />
-                        <p>Built for <br />
-                            <span>Apple Intelligence.</span></p>
-                    </div>
-                    <div>
-                        <img src="/battery.png" alt="Battery" />
-                        <p>Up to
-                            <span className="green-gradient">{' '}14 more hours{' '}</span>
-                            battery life.
-                            <span className="text-dark-100">{' '}(Up to 24 hours total.)
-                            </span></p>
+
+                    <div className="size-control">
+                        <div
+                            onClick={() => setScale(0.06)}
+                            className={clsx(scale === 0.06 ? 'bg-white text-black' : 'bg-transparent text-white')}
+                        >
+                            <p>14"</p>
+                        </div>
+                        <div
+                            onClick={() => setScale(0.08)}
+                            className={clsx(scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}
+                        >
+                            <p>16"</p>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
+                <StudioLights />
+
+                <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
+            </Canvas>
         </section>
     )
 }
-export default Highlights
+export default ProductViewer
