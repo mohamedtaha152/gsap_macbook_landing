2f140fdc5e362333a1fb392e0eed831d5da916e5
Mohamed Taha <69488789+mohamedtaha152@users.noreply.github.com>
2026-07-18 04:27:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import ProductViewer from "./components/ProductViewer"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 import Showcase from "./components/Showcase"
-
+import Performance from "./components/Performance"
+import Features from "./components/Features"
+import Highlights from "./components/Highlights"
+import Footer from "./components/Footer"
 
 
 gsap.registerPlugin(ScrollTrigger)
@@ -17,7 +20,10 @@ const App = () => {
     <Hero/>
     <ProductViewer/>
     <Showcase/>
-
+    <Performance/>
+    <Features/>
+    <Highlights/>
+    <Footer/>
 </main>
     )
 }
